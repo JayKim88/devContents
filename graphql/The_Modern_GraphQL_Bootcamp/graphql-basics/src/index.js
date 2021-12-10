d29424c7256dedd7b5f3397a61c445ec1a9f612f
JayKim88 <mearak43@gmail.com>
2021-12-10 23:58:08 +0900

    add: schemas and queries

--- graphql/The_Modern_GraphQL_Bootcamp/graphql-basics/src/index.js
+++ graphql/The_Modern_GraphQL_Bootcamp/graphql-basics/src/index.js
@@ -1,6 +1,6 @@
 import { GraphQLServer } from "graphql-yoga";
 
-const demoUsers = [
+const users = [
   {
     id: "123",
     name: "Jay",
@@ -19,35 +19,82 @@ const demoUsers = [
     email: "B@gmail.com",
   },
 ];
+
+const posts = [
+  {
+    id: "10",
+    title: "graphql",
+    body: "very fun to learn",
+    published: true,
+    author: "123",
+  },
+  {
+    id: "11",
+    title: "graphql12",
+    body: "very fun to play",
+    published: true,
+    author: "000",
+  },
+  {
+    id: "12",
+    title: "graphql123",
+    body: "very fun to dance",
+    published: true,
+    author: "000",
+  },
+];
+
 //Type definition (schema)
 const typeDefs = `
   type Query {
    users(query: String): [User!]!
+   posts(query: String): [Post!]!
    me: User!
    post: Post!
   }
+
   type User {
       id: ID!
       name: String!
       email: String!
       age: Int
   }
+
   type Post {
       id: ID!
       title: String!
       body: String!
       published: Boolean!
+      author: User!
   }
 `;
 //Resolver
 const resolvers = {
   Query: {
     users(parent, args, ctx, info) {
+      console.log(args);
+
       if (!args.query) {
-        return demoUsers;
+        return users;
       }
-      return demoUsers.filter((user) => {
+
+      return users.filter((user) => {
         return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
       });
     },
     me() {
@@ -65,23 +112,13 @@ const resolvers = {
         published: true,
       };
     },
-    // greeting(parent, args, ctx, info) {
-    //   if (args.name && args.job) {
-    //     return `Helllllo, ${args.job} ${args.name}`;
-    //   } else {
-    //     return "umm ?";
-    //   }
-    // },
-    // sumup(parent, args, ctx, info) {
-    //   if (args.numbers.length) {
-    //     return args.numbers.reduce((pre, cur) => pre + cur);
-    //   } else {
-    //     return 0;
-    //   }
-    // },
-    // grades(parent, args, ctx, info) {
-    //   return [10, 20, 30];
-    // },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
   },
 };
 
