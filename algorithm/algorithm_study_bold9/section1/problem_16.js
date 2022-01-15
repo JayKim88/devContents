8ab682fd64a1f56e772ebc17ecd981373b84060b
Jay Kim <mearak43@gmail.com>
2022-01-15 12:00:38 +0900

    add: section1 14 to 17 and section2 1

---
+++ algorithm/algorithm_study_bold9/section1/problem_16.js
@@ -0,0 +1,36 @@
+/*
+중복문자제거
+
+소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하 세요.
+제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+▣ 입력설명
+첫 줄에 문자열이 입력됩니다.
+▣ 출력설명
+첫 줄에 중복문자가 제거된 문자열을 출력합니다.
+▣ 입력예제 1 ksekkset
+▣ 출력예제 1 kset
+*/
+
+function solution(string) {
+  let result = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string.indexOf(string[i]) === i) result += string[i];
+  }
+  return result;
+}
+
+console.log(solution("ksekkset"));
+
+/*
+indexOf: 발견한 첫번째 인덱스를 반환.
+   function solution(s){  
+                let answer="";
+                //console.log(s.indexOf("K"));
+                for(let i=0; i<s.length; i++){
+                    //console.log(s[i], i, s.indexOf(s[i]));
+                    if(s.indexOf(s[i])===i) answer+=s[i];
+                }
+                return answer;
+            }
+            console.log(solution("ksekkset"));
+*/
